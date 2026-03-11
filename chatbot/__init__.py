e633e7230a2f47184613af8032e9ce391dd5e7b4
Guna Medisetty <128883236+GunaMedisetty9@users.noreply.github.com>
2026-03-11 20:12:39 +0530

    Import NLP components into chatbot module

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,3 +1,7 @@
 from utils.database import ChatDatabase
-
+from chatbot.nlp_engine import NLPEngine
+from chatbot.intent_classifier import IntentClassifier
+from chatbot.entity_extractor import EntityExtractor
+from chatbot.sentiment_analyzer import SentimentAnalyzer
+from chatbot.conversation_manager import ConversationManager
 __all__ = ['ChatDatabase']
